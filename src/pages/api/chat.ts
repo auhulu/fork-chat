f9a3a7dffbb217a40dbd514feeedd12371ab4b9e
auhulu <auhulu0@gmail.com>
2025-05-16 01:39:30 +0900

    fix

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -6,6 +6,7 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<{ message: Message }>,
 ) {
+	if (req.method !== "POST") return res.status(405);
 	const { messages } = req.body as { messages: Message[] };
 	const message = await getMessage(messages);
 	res.status(200).json({ message });
